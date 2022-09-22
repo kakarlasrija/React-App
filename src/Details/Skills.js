28db7f38756958cc37ec773b681d3ede938b93a8
kakarlasrija <srijakakarla8@gmail.com>
2022-09-23 00:02:39 +0530

    skills commit

--- src/Details/Skills.js
+++ src/Details/Skills.js
@@ -3,7 +3,79 @@ import "./Skills.css";
 function Skills() {
   return (
     <>
-      <div className="SkillsBdy"></div>
+      <div className="SkillsBdy">
+        <section className="PortfolioSkill" id="skill">
+          <div className="SkillHeading">
+            <div className="head">
+              <h1>
+                <b>My Skills</b>
+              </h1>
+            </div>
+          </div>
+          <div className="SkillsList">
+            <div className="Skills1">
+              <div>
+                <h3>React Js</h3>
+              </div>
+              <div>
+                <h3>HTML 5</h3>
+              </div>
+              <div>
+                <h3>CSS</h3>
+              </div>
+              <div>
+                <h3>Bootstrap</h3>
+              </div>
+              <div>
+                <h3>Xhtml/Jsp</h3>
+              </div>
+              <div>
+                <h3>JSF/Primefaces</h3>
+              </div>
+            </div>
+            <div className="Skills2">
+              <div>
+                <h3>JAVA</h3>
+              </div>
+              <div>
+                <h3>Spring Boot</h3>
+              </div>
+              <div>
+                <h3>Spring MVC</h3>
+              </div>
+              <div>
+                <h3>Hibernate</h3>
+              </div>
+              <div>
+                <h3>Spring Data Jpa</h3>
+              </div>
+              <div>
+                <h3>MySQL Server</h3>
+              </div>
+            </div>
+            <div className="Skills3">
+              <div>
+                <h3>Git</h3>
+              </div>
+              <div>
+                <h3>SVN</h3>
+              </div>
+              <div>
+                <h3>Maven/Gradle</h3>
+              </div>
+              <div>
+                <h3>Jenkins</h3>
+              </div>
+              <div>
+                <h3>MySQL</h3>
+              </div>
+              <div>
+                <h3>MicroServices</h3>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
     </>
   );
 }
